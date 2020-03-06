2a5287646494c73954b965108f6fed4bf26cf87a
dbond <debon1426@gmail.com>
2020-03-06 11:30:24 -0500

    to be continued..

--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,4 +1,29 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class SpeedComparator {
+import java.util.Comparator;
+
+public class SpeedComparator implements Comparator<Animal> {
+
+    Animal animal1;
+    Animal animal2;
+
+    SpeedComparator(){
+        BlueJay b = new BlueJay();
+        Horse h = new Horse();
+
+
+    }
+
+    @Override
+    public int compare(Animal animal1, Animal animal2) {
+
+        if (this.animal1.getSpeed() == this.animal2.getSpeed()) {
+            return 0;
+        } else if (animal1.getSpeed() > animal2.getSpeed()) {
+            return 1;
+        } else if (animal1.getSpeed() < animal2.getSpeed()) {
+            return 2;
+        }
+        return 0;
+    }
 }
